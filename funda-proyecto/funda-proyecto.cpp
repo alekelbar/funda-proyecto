fc83adc2470530bcf7845636daa18340903c7f92
alekelbar <alekelbar.personal@gmail.com>
2023-09-06 11:27:47 -0600

    banco

--- funda-proyecto/funda-proyecto.cpp
+++ funda-proyecto/funda-proyecto.cpp
@@ -2,10 +2,69 @@
 //
 
 #include <iostream>
-
+using namespace std;
+// #2 : realizar retiros, ingresos y consultas de los saldos de sus clientes.
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Declaración de variables
+	double saldoInicial = 1000.0;
+	double saldoActual = saldoInicial;
+	int opcion = 0;
+	double cantidad;
+
+	// Bucle principal para un menú
+	while (opcion != 4) {
+		// Menú de opciones
+		cout << "-------------BCR (Por que tampoco sirve xd)-------------" << endl;
+		cout << "Menu de opciones:" << endl;
+		cout << "1. Consultar saldo" << endl;
+		cout << "2. Realizar retiro" << endl;
+		cout << "3. Realizar deposito" << endl;
+		cout << "4. Salir" << endl;
+		cout << "Elija una opcion: ";
+		cin >> opcion;
+
+		system("cls");
+
+		switch (opcion) {
+		case 1:
+			// Consultar saldo
+			cout << "Saldo actual: $" << saldoActual << endl;
+			break;
+		case 2:
+			// Realizar retiro
+			cout << "Ingrese la cantidad a retirar: $";
+			cin >> cantidad;
+			if (cantidad <= saldoActual) {
+				saldoActual -= cantidad;
+				cout << "\nRetiro exitoso. Saldo actual: $" << saldoActual << endl;
+			}
+			else {
+				cout << "\nSaldo insuficiente para realizar el retiro." << endl;
+			}
+			break;
+		case 3:
+			// Realizar depósito
+			cout << "Ingrese la cantidad a depositar: $";
+			cin >> cantidad;
+			if (cantidad > 0) {
+				saldoActual += cantidad;
+				cout << "\nDepósito exitoso. Saldo actual: $" << saldoActual << endl;
+			}
+			else {
+				cout << "\nLa cantidad a depositar debe ser mayor que 0." << endl;
+			}
+			break;
+		case 4:
+			// Salir del programa
+			cout << "\nGracias por utilizar nuestros servicios." << endl;
+			return 0;
+		default:
+			cout << "\nOpción no válida. Por favor, seleccione una opción válida." << endl;
+		}
+	}
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
